118756b4f1d4eb57dd8bdff7e6934ee1fff2e47d
Sahil Naik <sahil7naik57@gmail.com>
2024-01-29 12:00:50 +0530

    Update Assignment-2.c

--- LP-V(HPC)/Assignment-2.c
+++ LP-V(HPC)/Assignment-2.c
@@ -1,21 +1,34 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 void swap();
 
+void *func(void *p) {
+      long tid;
+      tid = (long)p;
+      //printf("From the function, the thread id = %ld", pthread_self()); //get current thread id
+      printf("ID: %ld", pthread_self());
+      pthread_exit(NULL);
+}
+
 int main (int argc, char *argv[]) {
-	int SIZE =1<<8;
-	int A[SIZE];
-	for(int i=0;i<SIZE;i++)
+	long SIZE =1<<8;
+	long A[SIZE];
+
+	for(long i=0;i<SIZE;i++)
 	{
 	    A[i]=rand()%SIZE;
 	}
 	//int A[5] = {6,9,1,3,7};
-	int N = SIZE;
-	int i=0, j=0; 
-	int first;
+	long N = SIZE;
+    long i=0;
+	long j=0; 
+	long first;
 	double start,end;
+	int rc;
+    pthread_t thread[SIZE];
 	start=omp_get_wtime();
 	for( i = 0; i < N-1; i++ )
 	{
@@ -29,19 +42,22 @@ int main (int argc, char *argv[]) {
 			}
 		}
 	}
-end=omp_get_wtime();
+	end=omp_get_wtime();
 	for(i=0;i<N;i++)
 	{
-		printf(" %d",A[i]);
+                rc = pthread_create(&thread[i], NULL, func, (void *)i);
+                //printf("From the main function, the thread id = %ld", thread);
+                //pthread_join(thread, NULL); //join with main thread
+				printf(" %ld\n",A[i]);
 	}
 
-printf("\n-------------------------\n Time Parallel= %f",(end-start));
+	printf("\n-------------------------\n Time Parallel= %f",(end-start));
 }
 
 void swap(int *num1, int *num2)
 {
 
-	int temp = *num1;
+	long temp = *num1;
 	*num1 =  *num2;
 	*num2 = temp;
 }
